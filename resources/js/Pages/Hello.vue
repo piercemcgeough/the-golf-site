5a0cfcd567c8567ce5392ed515988be0c2415ca0
Pierce McGeough <pmcgeough@hotmail.co.uk>
2021-04-12 01:50:48 +0100

    Initial inertia vue working

---
+++ resources/js/Pages/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <p>Is anybody listening?</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
